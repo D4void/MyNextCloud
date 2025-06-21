253ab76d0c90d4b233e246c994c8b8d95f160403
D4void <d4void@m4he.fr>
2025-06-21 15:18:12 +0200

    update

--- mariadb-backup.sh
+++ mariadb-backup.sh
@@ -6,8 +6,8 @@ source $(dirname $0)/.env
 
 backupfile="${MARIADB_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").dump"
 
-#echo "Mise en maintenance Nextcloud"
-#
+echo "Mise en maintenance Nextcloud"
+docker exec nc-nextcloud php occ maintenance:mode --on
 
 echo "Backuping Mariadb ${MARIADB_DATABASE} database"
 #docker exec nc-db mariadb-dump -u ${MARIADB_USER} -p${MARIADB_PASSWORD} ${MARIADB_DATABASE} > /backup/${backupfile}
@@ -17,10 +17,11 @@ docker run --rm \
   --network MyNCnet \
   -e MYSQL_PWD=${MARIADB_PASSWORD} \
   mariadb:11.4-noble \
-  sh -c "echo ${MARIADB_USER}; echo ${MARIADB_DATABASE}; mariadb-dump -h nc-db -u ${MARIADB_USER} ${MARIADB_DATABASE}" \
-  > ${backupfile}
+  sh -c "mariadb-dump -h nc-db -u ${MARIADB_USER} ${MARIADB_DATABASE}" \
+  > backup/${backupfile}
 
-#echo "Fin maintenance Nextcloud"
-#
+echo "Fin maintenance Nextcloud"
+docker exec nc-nextcloud php occ maintenance:mode --off
+
 
 echo "End."
