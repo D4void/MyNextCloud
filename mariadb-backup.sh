c5f4c9611f1bba5e2318aad29db1f4948eb3e725
D4void <d4void@m4he.fr>
2025-06-21 14:37:12 +0200

    backup test

--- mariadb-backup.sh
+++ mariadb-backup.sh
@@ -10,7 +10,14 @@ backupfile="${MARIADB_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").dump"
 #
 
 echo "Backuping Mariadb ${MARIADB_DATABASE} database"
-docker exec nc-db mariadb-dump -u ${MARIADB_USER} -p {MARIADB_PASSWORD} ${MARIADB_DATABASE} > /backup/${backupfile}
+#docker exec nc-db mariadb-dump -u ${MARIADB_USER} -p{MARIADB_PASSWORD} ${MARIADB_DATABASE} > /backup/${backupfile}
+
+docker run --rm \
+  --name mariadb-dump \
+  --network ncnet \
+  -e MYSQL_PWD=motdepasse \
+  mariadb:latest \
+  sh -c 'mariadb-dump -h nc-db -u ${MARIADB_USER} -p{MARIADB_PASSWORD} ${MARIADB_DATABASE}'
 
 #echo "Fin maintenance Nextcloud"
 #
