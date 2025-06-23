90402e29865eed501788776a7fa3c1269471d274
D4void <d4void@m4he.fr>
2025-06-23 18:02:52 +0200

    correction

--- init-voldir.sh
+++ init-voldir.sh
@@ -4,7 +4,7 @@
 source .env
 
 if [[ ! -d ${NC_VOL}  ]]; then
-    mkdir -p ${NC_VOL}/{var-lib-mysql,data,config,apps,backup}
+    mkdir -p ${NC_VOL}/{var-lib-mysql,data,config,custom_apps,backup}
     
     chmod -R 777 ${NC_VOL}
 
