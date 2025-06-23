b5e2fa4adaa1e7a4e105ec27ed28d0217badee3e
D4void <d4void@m4he.fr>
2025-06-23 15:41:38 +0200

    backup

--- mariadb-backup.sh
+++ mariadb-backup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Script to make a dump of a Mariadb Nextcloud database running in a Docker container
-# The dump is dropped via a volume mounted on /backup
 
 source $(dirname $0)/.env
 
 backupfile="${MARIADB_DATABASE}_dump_$(date +"%Y-%m-%d_%Hh%Mm%S").dump"
 
-echo "Mise en maintenance Nextcloud"
+echo "Begin Nextcloud maintenance"
 docker exec nc-nextcloud php occ maintenance:mode --on
 
 sleep 5
@@ -17,13 +16,13 @@ docker run --rm \
   --network MyNCnet \
   -e MYSQL_PWD=${MARIADB_PASSWORD} \
   mariadb:11.4-noble \
-  sh -c "mariadb-dump --single-transaction -h nc-db -u ${MARIADB_USER} ${MARIADB_DATABASE}" \
+  sh -c "mariadb-dump --single-transaction --default-character-set=utf8mb4 -h nc-db -u ${MARIADB_USER} ${MARIADB_DATABASE}" \
   > backup/${backupfile}
 
 
 sleep 5
 
-echo "Fin maintenance Nextcloud"
+echo "End Nextcloud maintenance"
 docker exec nc-nextcloud php occ maintenance:mode --off
 
 
--- mariadb-restore.sh
+++ mariadb-restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script to restore a dump of a Mariadb Nextcloud database running in a docker container
-# The dump is accessible via the volume mounted on /backup
+
 
 source $(dirname $0)/.env
 
@@ -9,14 +9,30 @@ if [[ -f "$1" ]]; then
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        #echo "Mise en maintenance Nextcloud"
-        #
+        echo "Begin Nextcloud maintenance"
+        docker exec nc-nextcloud php occ maintenance:mode --on
+
+        sleep 5
+
+        echo "Delete and create Nextcloud database"
+        docker run --rm \
+            --name mariadb \
+            --network MyNCnet \
+            -e MYSQL_PWD=${MARIADB_PASSWORD} \
+            mariadb:11.4-noble \
+            sh -c "mariadb -h nc-db -u ${MARIADB_USER} -e 'DROP DATABASE ${MARIADB_DATABASE}; \
+                CREATE DATABASE ${MARIADB_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci' "
 
         echo "Restoring backup $1 into Mariadb"
-        docker exec nc-db mariadb-dump -u ${MARIADB_USER} -p {MARIADB_PASSWORD} ${MARIADB_DATABASE} < /backup/$1
+        docker run --rm \
+            --name mariadb-dump \
+            --network MyNCnet \
+            -e MYSQL_PWD=${MARIADB_PASSWORD} \
+            mariadb:11.4-noble \
+            sh -c "mariadb -h nc-db -u ${MARIADB_USER} ${MARIADB_DATABASE}" < $1
 
-        #echo "Fin maintenance Nextcloud"
-        #
+        echo "End Nextcloud maintenance"
+        docker exec nc-nextcloud php occ maintenance:mode --off
 
     fi
 else
