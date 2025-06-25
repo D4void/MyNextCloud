beba1389a2f18eb8f4030d00313336433f996087
D4void <d4void@m4he.fr>
2025-06-25 15:18:50 +0200

    update volume

--- init-voldir.sh
+++ init-voldir.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-# Script to init docker volumes directories
+# Script to init docker volumes host directories, set uid/gid and rights
+# 
+# docker exec -it nc-nextcloud id www-data
+# docker exec -it nc-db id mysql
 
 source .env
 
 if [[ ! -d ${NC_VOL}  ]]; then
-    mkdir -p ${NC_VOL}/{var-lib-mysql,data,config,custom_apps,backup}
     
-    chmod -R 777 ${NC_VOL}
+    mkdir -p ${NC_VOL}/{var-lib-mysql,data,config,custom_apps,backup}
+    chown -R 999:999 ${NC_VOL}/var-lib-mysql
+    chown -R 33:33 ${NC_VOL}/{data,config,custom_apps}
+
+    chmod -R 750 ${NC_VOL}/{data,config,custom_apps,backup}
+    chmod -R 700 ${NC_VOL}/var-lib-mysql
+    chmod 755 ${NC_VOL}
 
 fi
