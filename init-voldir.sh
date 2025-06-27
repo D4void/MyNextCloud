e4c5b254faa48d40c5c6d75a1ff76adadd599740
D4void <d4void@m4he.fr>
2025-06-27 11:27:54 +0200

    update volume

--- init-voldir.sh
+++ init-voldir.sh
@@ -8,11 +8,11 @@ source .env
 
 if [[ ! -d ${NC_VOL}  ]]; then
     
-    mkdir -p ${NC_VOL}/{var-lib-mysql,data,config,custom_apps,backup}
+    mkdir -p ${NC_VOL}/{var-lib-mysql,data,config,app,custom_apps,backup}
     chown -R 999:999 ${NC_VOL}/var-lib-mysql
-    chown -R 33:33 ${NC_VOL}/{data,config,custom_apps}
+    chown -R 33:33 ${NC_VOL}/{data,config,app,custom_apps}
 
-    chmod -R 750 ${NC_VOL}/{data,config,custom_apps,backup}
+    chmod -R 750 ${NC_VOL}/{data,config,app,custom_apps,backup}
     chmod -R 700 ${NC_VOL}/var-lib-mysql
     chmod 755 ${NC_VOL}
 
